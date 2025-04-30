3f6fd711788abbfb3cd4a83e87e700fbccae1cdc
foucos <namarsco@yahoo.com>
2025-05-01 02:49:31 +0300

    Update sql_init

--- sql_init.sql
+++ sql_init.sql
@@ -1,36 +1,54 @@
 --Create table queries
 
-CREATE TABLE mm_user (
-    UserID INT AUTO_INCREMENT PRIMARY KEY, Fname VARCHAR(50) NOT NULL,
-    Lname VARCHAR(50) NOT NULL,   email VARCHAR(50) NOT NULL,
-    role VARCHAR(50) NOT NULL, password VARCHAR(100) NOT NULL,
-   CreatedTime DATETIME NOT NULL) ENGINE=MyISAM;
+CREATE TABLE `mm_user` (
+  `UserID` int NOT NULL,
+  `Fname` varchar(50) NOT NULL,
+  `Lname` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `CreatedTime` datetime NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE mm_experiment (
-    ExperimentID INT AUTO_INCREMENT PRIMARY KEY,  ExperimentName VARCHAR(100) NOT NULL,
-    StartDate DATETIME NOT NULL, EndDate DATETIME NOT NULL,
-    Description VARCHAR(100) NOT NULL, CreatedTime DATETIME NOT NULL,
-    UpdatedDate DATETIME NULL) ENGINE=MyISAM;
+CREATE TABLE `mm_experiment` (
+  `ExperimentID` int NOT NULL,
+  `ExperimentCode` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
+  `StartDate` datetime NOT NULL,
+  `EndDate` datetime NOT NULL,
+  `Description` varchar(250) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `CreatedTime` datetime NOT NULL,
+  `UpdatedDate` datetime DEFAULT NULL,
+  `Creator_UserID` int NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE mm_media (
-    MediaID INT AUTO_INCREMENT PRIMARY KEY, FilePath VARCHAR(100) NOT NULL,
-    AutomobileType VARCHAR(50) NOT NULL,  Duration FLOAT NOT NULL,
-    CreatedTime DATETIME NOT NULL,  ExperimentID INT NOT NULL,
-    FOREIGN KEY (ExperimentID) REFERENCES mm_experiment(ExperimentID)) ENGINE=MyISAM;
+CREATE TABLE `mm_media` (
+  `MediaID` int NOT NULL,
+  `FilePath` varchar(100) NOT NULL,
+  `AutomobileType` varchar(50) NOT NULL,
+  `Duration` float DEFAULT NULL,
+  `CreatedTime` datetime NOT NULL,
+  `ExperimentID` int NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE mm_question (
-    QuestionID INT AUTO_INCREMENT PRIMARY KEY,  QuestionText VARCHAR(1000) NOT NULL,
-    QuestionType VARCHAR(50) NOT NULL,   CreatedTime DATETIME NOT NULL,
-    ExperimentID INT NOT NULL,    MediaID INT NOT NULL,
-    FOREIGN KEY (ExperimentID) REFERENCES mm_experiment(ExperimentID),
-    FOREIGN KEY (MediaID) REFERENCES mm_media(MediaID)) ENGINE=MyISAM;
+CREATE TABLE `mm_question` (
+  `QuestionID` int NOT NULL,
+  `QuestionText` varchar(1000) NOT NULL,
+  `QuestionType` varchar(50) NOT NULL,
+  `CreatedTime` datetime NOT NULL,
+  `ExperimentID` int NOT NULL,
+  `MediaID` int NOT NULL,
+  `Choices` text
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE mm_response (
-    ResponseID INT AUTO_INCREMENT PRIMARY KEY,  Value INT NOT NULL,
-    VehicleGuess VARCHAR(50) NOT NULL,    CorrectGuess CHAR(1) NOT NULL,
-    CreatedTime DATETIME NOT NULL,    Duration DATETIME NOT NULL,
-    UserID INT NOT NULL,    QuestionID INT NOT NULL,    FOREIGN KEY (UserID) REFERENCES mm_user(UserID),
-    FOREIGN KEY (QuestionID) REFERENCES mm_question(QuestionID)) ENGINE=MyISAM;
+CREATE TABLE `mm_response` (
+  `ResponseID` int NOT NULL,
+  `Answer` text NOT NULL,
+  `Duration` int NOT NULL,
+  `ExperimentID` int NOT NULL,
+  `QuestionID` int NOT NULL,
+  `CreatedTime` datetime NOT NULL,
+  `UserID` int NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Analytical queries
 
